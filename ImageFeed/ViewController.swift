555716fdb7d886c7eddee2f5172fd6f0e423d60f
Mikita Ivanou <ivanovnk30@gmail.com>
2026-01-02 20:08:15 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Mikita Ivanou on 2.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

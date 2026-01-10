5d229d18d1c0af9016424abd70a820787242a905
mikitaivanou <ivanovnk30@gmail.com>
2026-01-10 13:02:41 +0300

    настроены аутлеты кастомных элементов в классе ImageListCell

--- ImageFeed/imageList/ImagesListCell.swift
+++ ImageFeed/imageList/ImagesListCell.swift
@@ -8,5 +8,11 @@
 import UIKit
  
 final class ImagesListCell: UITableViewCell {
+    @IBOutlet weak var cellDataLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    @IBOutlet weak var cellButtonImage: UIButton!
+    @IBAction func cellButton(_ sender: Any) {
+    }
     static let reuseIdentifier = "ImagesListCell"
 }

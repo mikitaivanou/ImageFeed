bfe29aa2751ff67bc4167ed8e32435e2f9299eaa
mikitaivanou <ivanovnk30@gmail.com>
2026-01-10 12:36:47 +0300

    настроена кастомная ячейка и выставлены констрейты для кнопки, лейбла и картинки. коммит после урока: Ссылка на урок: [Спринт 8/26 → Тема 4/5: Практика по работе с таблицей в проекте → Урок 4/5: Создаём UITableViewCell и связь с таблицей]( https://practicum.yandex.ru/learn/ios-developer-plus/courses/c64ef9f2-2f0c-4b60-b64b-6923467542ea/sprints/580568/topics/3696318c-d362-42ad-8140-d3ee5bf12cdd/lessons/3f9c1476-e151-4ab0-8564-1a33090ee8ea/ )

--- ImageFeed/imageList/ImagesListViewController.swift
+++ ImageFeed/imageList/ImagesListViewController.swift
@@ -11,7 +11,7 @@ class ImagesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.rowHeight = 200
     }
 
     @IBOutlet private var tableView: UITableView!

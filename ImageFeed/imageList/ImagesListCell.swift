406d6a6b79296b0b36bb1d329a7d9aecb470483d
mikitaivanou <ivanovnk30@gmail.com>
2026-01-10 19:31:09 +0300

    созданы аутлеты для: лейбл даты, кнопки, изображения. создан экшн для кнопки настроен градиент для ячейки

--- ImageFeed/imageList/ImagesListCell.swift
+++ ImageFeed/imageList/ImagesListCell.swift
@@ -15,4 +15,58 @@ final class ImagesListCell: UITableViewCell {
     @IBAction func cellButton(_ sender: Any) {
     }
     static let reuseIdentifier = "ImagesListCell"
+    
+    
+    
+    
+    
+    
+    
+    private let gradientLayer = CAGradientLayer()
+    private let gradientHeight: CGFloat = 30
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupGradient()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        gradientLayer.frame = CGRect(
+            x: 0,
+            y: contentView.bounds.height - gradientHeight,
+            width: contentView.bounds.width,
+            height: gradientHeight
+        )
+    }
+
+    private func setupGradient() {
+       
+        gradientLayer.colors = [
+            UIColor(hex: "#1A1B22", alpha: 0.0).cgColor, // 0%
+            UIColor(hex: "#1A1B22", alpha: 1.0).cgColor  // 100%
+        ]
+
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint   = CGPoint(x: 0.5, y: 1.0)
+
+            // contentView.layer.addSublayer(gradientLayer)
+        contentView.layer.insertSublayer(gradientLayer, at: 1)
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        let hex = hex.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: hex)
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let r = CGFloat((rgb >> 16) & 0xFF) / 255
+        let g = CGFloat((rgb >> 8) & 0xFF) / 255
+        let b = CGFloat(rgb & 0xFF) / 255
+
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
 }
--- ImageFeed/imageList/ImagesListViewController.swift
+++ ImageFeed/imageList/ImagesListViewController.swift
@@ -12,22 +12,53 @@ class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 
     @IBOutlet private var tableView: UITableView!
-    func configCell(for cell: ImagesListCell) { }
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+
+        cell.cellImage.image = image
+        cell.cellDataLabel.text = dateFormatter.string(from: Date())
+
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        cell.cellButtonImage.setImage(likeImage, for: .normal)
+    }
     
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
         
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -37,7 +68,7 @@ extension ImagesListViewController: UITableViewDataSource {
                     return UITableViewCell()
                 }
                 
-                configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
                 return imageListCell
     }
     

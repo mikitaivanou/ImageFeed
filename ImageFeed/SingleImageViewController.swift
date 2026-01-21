bfc79f03de85b3f37b1c8f47f01c17a513ae5755
mikitaivanou <ivanovnk30@gmail.com>
2026-01-21 21:51:09 +0300

    - создан VC Single Image, размещена imageView и настроены констрейты

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Mikita Ivanou on 20.01.26.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+}

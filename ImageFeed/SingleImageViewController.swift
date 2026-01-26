420e9eced2144146ba18571ed76d0fac78c8e35f
mikitaivanou <ivanovnk30@gmail.com>
2026-01-26 21:24:44 +0300

    - настроен переход через идентификатор segue; - настроена кнопка для возвращения к списку изображений; - настроено отображения в развернутом виде для выбранной картинки. Выполнено задание по уроку: Ссылка на урок: [Спринт 9/26 → Тема 2/6: Навигация → Урок 7/7: Present View Controller]( https://practicum.yandex.ru/learn/ios-developer-plus/courses/c64ef9f2-2f0c-4b60-b64b-6923467542ea/sprints/644164/topics/d96874b8-526a-463b-be5f-d8829a94bc62/lessons/81698b79-aca0-4189-b4b5-aa4aea4dc235/#62ece08f-c5da-406b-b054-8724c455c2fa ) Вопрос: 

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 final class SingleImageViewController: UIViewController {
     
-
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else {return}
+            imageView.image = image
+        }
+    }
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }

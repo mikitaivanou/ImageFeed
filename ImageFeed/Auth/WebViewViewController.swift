ac658a9146cf3a1acd17fa8e6694d217a06dd1da
mikita_ivanou <ivanovnk30@gmail.com>
2026-02-15 21:30:26 +0300

    - выполнены задания по уроку: Ссылка на урок: [Спринт 10/26 → Тема 4/8: Авторизация: Web view → Урок 2/2: WebView: практика]( https://practicum.yandex.ru/learn/ios-developer-plus/courses/c64ef9f2-2f0c-4b60-b64b-6923467542ea/sprints/644165/topics/c7028312-445e-46f0-9279-4cc04313f7f8/lessons/8e43dd86-e679-4779-96f0-c785aa00d7af/#9d13d965-d442-48e4-912b-8fe31b86a821 ) - создан VC входа - создан VC для WView - настроен segue перехода нажатия кнопки на страницу регистрации - настроена кнопка перехода назад со страницы регистрации

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,74 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Mikita Ivanou on 15/02/2026.
+//
+import UIKit
+import WebKit
+
+class WebViewViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
+    
+    enum WebViewConstants {
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAuthView()
+        webView.navigationDelegate = self
+    }
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString)
+        else {return}
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),                   //4
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url
+        else {return}
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView( _ webView: WKWebView,
+                  decidePolicyFor navigationAction: WKNavigationAction,
+                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let code = code(from: navigationAction) {
+            delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            decisionHandler(.cancel)
+        }
+        else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if let url = navigationAction.request.url,
+           let urlComponents = URLComponents(string: url.absoluteString),
+           urlComponents.path == "/oauth/authorize/native",
+           let items = urlComponents.queryItems,
+           let codeItem = items.first(where: {$0.name == "code"}) {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+}
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Mikita Ivanou on 15/02/2026.
+//
+
+import Foundation
+
+
+    enum Constants {
+        static let accessKey = "4rg2uEGFWB6N1yqicLiejqQsG1U3DtnJOvT7Zsy8o38"
+        static let secretKey = "CjTkPX8OfUl_WAz4IWU6aymfAXa1jz4HKLgAP9YYr3o"
+        static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+        static let accessScope = "public+read_user+write_likes"
+        static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    }

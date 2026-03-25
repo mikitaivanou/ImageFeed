21bf87c326b081967f73241c7492ad8fe113df4a
mikita_ivanou <ivanovnk30@gmail.com>
2026-03-25 20:05:50 +0300

    - второй тест не проходит

--- Image FeedTests/WebViewTests.swift
+++ Image FeedTests/WebViewTests.swift
@@ -23,4 +23,21 @@ final class WebViewTests: XCTestCase {
         //then
         XCTAssertTrue(presenter.viewDidLoadCalled) //behaviour verification
     }
+    
+    
+    func testPresenterCallsLoadRequest() {
+        //given
+        let viewController = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        presenter.viewDidLoad()
+        
+        //then
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+    
 }

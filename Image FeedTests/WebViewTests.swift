0c0dc6fc868e522eba15cfe255973524ab25363b
mikita_ivanou <ivanovnk30@gmail.com>
2026-03-25 20:52:31 +0300

    - проходят тесты до 4 задания

--- Image FeedTests/WebViewTests.swift
+++ Image FeedTests/WebViewTests.swift
@@ -25,10 +25,10 @@ final class WebViewTests: XCTestCase {
     }
     
     
-    func testPresenterCallsLoadRequest() {
+    func testPresenterCallsLoadRequest() async {
         //given
         let viewController = WebViewViewControllerSpy()
-        let authHelper = AuthHelper()
+        let authHelper = await MainActor.run { AuthHelper() }
         let presenter = WebViewPresenter(authHelper: authHelper)
         viewController.presenter = presenter
         presenter.view = viewController
@@ -38,6 +38,69 @@ final class WebViewTests: XCTestCase {
         
         //then
         XCTAssertTrue(viewController.loadRequestCalled)
+    }
+    
+    func testProgressVisibleWhenLessThenOne() async {
+        //given
+//        let authHelper = AuthHelper()
+        let authHelper = await MainActor.run { AuthHelper() }
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProgressHiddenWhenOne() async{
+        //given
+//        let authHelper = AuthHelper() //Dummy
+        let authHelper = await MainActor.run { AuthHelper() }
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress) // return value verification
+        
+        //then
+        XCTAssertTrue(shouldHideProgress)
+    }
+    
+    func testAuthHelperAuthURL() async {
+        //given
+        let configuration = AuthConfiguration.standard
+//        let authHelper = AuthHelper(configuration: configuration)
+        let authHelper = await MainActor.run { AuthHelper(configuration: configuration) }
+        //when
+        let url = authHelper.authURL()
+
+        guard let urlString = url?.absoluteString else {
+            XCTFail("Auth URL is nil")
+            return
+        }
+
+        //then
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+    }
+    
+    func testCodeFromURL() async {
+        //given
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")!
+        urlComponents.queryItems = [URLQueryItem(name: "code", value: "test code")]
+        let url = urlComponents.url!
+//        let authHelper = AuthHelper()
+        let authHelper = await MainActor.run { AuthHelper() }
+        //when
+        let code = authHelper.code(from: url)
+        
+        //then
+        XCTAssertEqual(code, "test code")
     }
     
 }
